40a673e36d054ec55c7216f44108dbd17fadf19e
Ilias Safiullin <r0832668@student.thomasmore.be>
2022-02-28 16:24:59 +0100

    inline code

--- kata-java-start/src/main/java/be/codekata/CodeShouldGoHere.java
+++ kata-java-start/src/main/java/be/codekata/CodeShouldGoHere.java
@@ -1,4 +1,30 @@
 package be.codekata;
 
-public class CodeShouldGoHere {
+//public class CodeShouldGoHere {
+//}
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class HelloSelenium {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:/Users/Ilias/Downloads/chromedriver_win32/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.be/");
+        WebElement search = driver.findElement(By.name("q"));
+//        WebElement button = driver.findElement(By.name("btnK"));
+        WebElement agree = driver.findElement(By.id("L2AGLb"));
+
+        agree.click();
+
+        search.sendKeys("gg");
+
+        search.submit();
+
+        driver.quit();
+    }
 }
+
--- kata-java-start/src/test/java/be/codekata/TestsShouldGoHere.java
+++ kata-java-start/src/test/java/be/codekata/TestsShouldGoHere.java
@@ -2,6 +2,11 @@ package be.codekata;
 
 import org.junit.jupiter.api.Test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
@@ -15,5 +20,35 @@ public class TestsShouldGoHere {
     @Test
     public void passingTest(){
         assertThat(true,equalTo(true));
+    }
+
+    @Test
+    public void searchTest() throws InterruptedException {
+        setChromeDriverLocation();
+        WebDriver driver = new  ChromeDriver();
+
+        driver.get("https://www.hln.be/");
+
+        driver.switchTo().frame("sp_message_iframe_609148");
+
+        WebElement cookies = driver.findElement(By.xpath("//*[@id=\"notice\"]/div[3]/button[1]"));
+        cookies.click();
+
+
+        WebElement searchButton = driver.findElement(By.xpath("/html/body/header/section[3]/div[1]/div/nav/div[1]/div/label/span"));
+        searchButton.click();
+
+        WebElement searchtab = driver.findElement(By.xpath("/html/body/header/section[3]/div[2]/div[2]/div/form/input"));
+        searchtab.sendKeys("turnhout");
+
+        WebElement submitButton = driver.findElement(By.className("submit"));
+        submitButton.click();
+
+        driver.quit();
+
+    }
+
+    private void setChromeDriverLocation() {
+        System.setProperty("webdriver.chrome.driver","C:/Users/Ilias/Downloads/chromedriver_win32/chromedriver.exe");
     }
 }
