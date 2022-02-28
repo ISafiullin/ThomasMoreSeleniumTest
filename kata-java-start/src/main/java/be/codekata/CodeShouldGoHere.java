3500d4ad4aa70fa4782407cac2a036f967b78a1d
Ilias Safiullin <r0832668@student.thomasmore.be>
2022-02-28 16:39:11 +0100

    inline code met 1 class

--- kata-java-start/src/main/java/be/codekata/CodeShouldGoHere.java
+++ kata-java-start/src/main/java/be/codekata/CodeShouldGoHere.java
@@ -1,30 +1,35 @@
 package be.codekata;
 
-//public class CodeShouldGoHere {
-//}
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+//public class CodeShouldGoHere {
+//}
+
 class HelloSelenium {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","C:/Users/Ilias/Downloads/chromedriver_win32/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new  ChromeDriver();
 
-        driver.get("https://www.google.be/");
-        WebElement search = driver.findElement(By.name("q"));
-//        WebElement button = driver.findElement(By.name("btnK"));
-        WebElement agree = driver.findElement(By.id("L2AGLb"));
+        driver.get("https://www.hln.be/");
 
-        agree.click();
+        driver.switchTo().frame("sp_message_iframe_609148");
 
-        search.sendKeys("gg");
+        WebElement cookies = driver.findElement(By.xpath("//*[@id=\"notice\"]/div[3]/button[1]"));
+        cookies.click();
 
-        search.submit();
+
+        WebElement searchButton = driver.findElement(By.xpath("/html/body/header/section[3]/div[1]/div/nav/div[1]/div/label/span"));
+        searchButton.click();
+
+        WebElement searchtab = driver.findElement(By.xpath("/html/body/header/section[3]/div[2]/div[2]/div/form/input"));
+        searchtab.sendKeys("turnhout");
+
+        WebElement submitButton = driver.findElement(By.className("submit"));
+        submitButton.click();
 
         driver.quit();
     }
 }
-
